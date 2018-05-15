bcce16c4aa28195dea5a22f992bcfb0525f159d6
colinjpaul <colinpaul@gmail.com>
2018-05-15 22:03:44 +0100

    store the card

--- ConsoleGolf/scores.py
+++ ConsoleGolf/scores.py
@@ -1,2 +1,14 @@
+import sqlite3
+conn = sqlite3.connect('scorecard.sqlite')
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS scores
+    (id INT PRIMARY KEY, Hole INT UNIQUE, Par INT, 
+    Handicap INT UNIQUE, Score INT)''')
+
 score = input("Enter your score > ")
 print("You scored ", score)
+
+ins = 'INSERT INTO scores (Score) VALUES(?)'
+cur.execute(ins, (5))
+
