6fbd95080bfc490a4d8e674747b308a5bff63926
Colin <Colin_Paul@McAfee.com>
2018-05-16 13:16:52 +0100

    store details for one hole in DB
    gotta start somewhere!

--- ConsoleGolf/scores.py
+++ ConsoleGolf/scores.py
@@ -1,14 +1,22 @@
 import sqlite3
-conn = sqlite3.connect('scorecard.sqlite')
-cur = conn.cursor()
 
-cur.execute('''CREATE TABLE IF NOT EXISTS scores
-    (id INT PRIMARY KEY, Hole INT UNIQUE, Par INT, 
-    Handicap INT UNIQUE, Score INT)''')
 
-score = input("Enter your score > ")
-print("You scored ", score)
+db = sqlite3.connect('scorecard.sqlite')
+cur = db.cursor()
 
-ins = 'INSERT INTO scores (Score) VALUES(?)'
-cur.execute(ins, (5))
+hole = 1
+par = 4
+score = 5
+
+cur.execute('''CREATE TABLE scores(Hole INT UNIQUE, Par INT, Score INT)''')
+
+# score = input("Enter your score > ")
+# print("You scored ", score)
+
+cur.execute('''INSERT INTO scores(Hole, Par, Score)
+            VALUES(?,?,?)''', (hole, par, score))
+
+db.commit()
+
+
 
