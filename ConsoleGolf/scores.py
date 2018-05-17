4ef0473581783bb9f015551aba68424470ff849f
Colin <Colin_Paul@McAfee.com>
2018-05-17 13:41:05 +0100

    retrieving from db

--- ConsoleGolf/scores.py
+++ ConsoleGolf/scores.py
@@ -4,19 +4,29 @@ import sqlite3
 db = sqlite3.connect('scorecard.sqlite')
 cur = db.cursor()
 
-hole = 1
-par = 4
-score = 5
+hole = 2
+par = 3
+score = 4
 
-cur.execute('''CREATE TABLE scores(Hole INT UNIQUE, Par INT, Score INT)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS scores(Hole INT UNIQUE, Par INT, Score INT)''')
 
 # score = input("Enter your score > ")
 # print("You scored ", score)
 
-cur.execute('''INSERT INTO scores(Hole, Par, Score)
-            VALUES(?,?,?)''', (hole, par, score))
+# cur.execute('''INSERT INTO scores(Hole, Par, Score)
+#             VALUES(?,?,?)''', (hole, par, score))
 
-db.commit()
+
+
+cur.execute('''SELECT hole, par, score FROM scores''')
+# one_hole = cur.fetchone()
+# print(one_hole[2])
+all_rows = cur.fetchall()
+for row in all_rows:
+    print('Hole: {0}, Par: {1}, Score: {2} '.format(row[0], row[1], row[2]))
+
+
+
 
 
 
