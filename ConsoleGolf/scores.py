0faf006a3abd40db670dffbc4eee3d487db41ceb
Colin <Colin_Paul@McAfee.com>
2018-05-24 13:31:33 +0100

    working with pandas

--- ConsoleGolf/scores.py
+++ ConsoleGolf/scores.py
@@ -2,32 +2,29 @@ import sqlite3
 import os
 import pandas as pd
 
-home = os.path.dirname("C:/Users/cpaul/Documents/GitHub/OutofBounds/")
+home = os.path.dirname("C:/Users/cpaul/Documents/GitHub/OutofBounds/Data/")
 abs_file_path = os.path.join(home, "Scorecard.csv")
-
+#
 df = pd.read_csv(abs_file_path)
+
+hole = df.Hole(0)
+
+
 
 db = sqlite3.connect('scorecard.sqlite')
 cur = db.cursor()
 
-hole = 2
-par = 3
-score = 4
-
 cur.execute('''CREATE TABLE IF NOT EXISTS scores(Hole INT UNIQUE, Par INT, Score INT)''')
-
-# score = input("Enter your score > ")
-# print("You scored ", score)
 
 # cur.execute('''INSERT INTO scores(Hole, Par, Score)
 #             VALUES(?,?,?)''', (hole, par, score))
-
-cur.execute('''SELECT hole, par, score FROM scores''')
-# one_hole = cur.fetchone()
-# print(one_hole[2])
-all_rows = cur.fetchall()
-for row in all_rows:
-    print('Hole: {0}, Par: {1}, Score: {2} '.format(row[0], row[1], row[2]))
+#
+# cur.execute('''SELECT hole, par, score FROM scores''')
+# # one_hole = cur.fetchone()
+# # print(one_hole[2])
+# all_rows = cur.fetchall()
+# for row in all_rows:
+#     print('Hole: {0}, Par: {1}, Score: {2} '.format(row[0], row[1], row[2]))
 
 
 
